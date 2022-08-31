4478f49cfc4c1f2e5be8c7ca299d51ac4c2ad357
Gökay <ygokayo@hotmail.com>
2022-08-31 05:05:42 +0300

    minor fixes

--- Assets/_Scripts/GameCore/UI/HoldButton.cs
+++ Assets/_Scripts/GameCore/UI/HoldButton.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
